2735cbbb73ed0a64a31fe819598ddba2d2d5c7eb
rud-fintech <rvv220001@utdallas.edu>
2025-03-20 23:39:32 -0500

    About Us page styling

--- src/Componets/Login/About.jsx
+++ src/Componets/Login/About.jsx
@@ -1,9 +1,34 @@
-const About = () => {
-    return (
-        <>
-            <p>Hello</p>
-        </>
-    );
-};
+import React from 'react';
+import '../styles/about.css';
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <div className="team">
+        <div className="team-member">
+          <img src="./src/assets/img.jpg" alt="Member" />
+          <h3>Mohan</h3>
+          <p>2 Line Description</p>
+        </div>
+        <div className="team-member">
+          <img src="./src/assets/img.jpg" alt="Member" />
+          <h3>Kaushik</h3>
+          <p>2 Line Description</p>
+        </div>
+        <div className="team-member">
+          <img src="./src/assets/img.jpg" alt="Member" />
+          <h3>Rudra</h3>
+          <p>2 Line Description</p>
+        </div>
+        <div className="team-member">
+          <img src="./src/assets/img.jpg" alt="Member" />
+          <h3>Viswa</h3>
+          <p>2 Line Description</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default About;
