cb8c54ec965c854926b1703682fb38bbf5fa070e
rud-fintech <rvv220001@utdallas.edu>
2025-05-06 00:49:22 -0500

    3rd session

--- Data Base/App/Services/user_dao.py
+++ Data Base/App/Services/user_dao.py
@@ -2,6 +2,9 @@ from App.db import get_session
 from App.Models.user import User
 
 def create_user(username, password, balance):
+    try:
+        balance= int(balance)
+    except Exception as e:
     user = User(username=username,password=password,balance=balance)
     with get_session() as session:
         session.add(user)
