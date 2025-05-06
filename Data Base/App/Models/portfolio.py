b49e0eb1d3c8339ac2effe4150c8fab7e8c24f74
rud-fintech <rvv220001@utdallas.edu>
2025-05-05 23:05:14 -0500

    test2

--- Data Base/App/Models/portfolio.py
+++ Data Base/App/Models/portfolio.py
@@ -1,14 +1,13 @@
-from App.db import Base
 from sqlalchemy import Column, String, Integer
+from app.extensions import db
 
-class Portfolio(Base):
-    __tablename__ = 'Portfolio'
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    strategy = Column(String, nullable=False)
-    userId = Column(Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    strategy = db.Column(db.String(100), nullable=False)
+    userID = db.Column(db.Integer, nullable=False)  # You might add a ForeignKey later
 
-
-def __str__(self):
-        return f'[id: {self.id}, name: {self.name}, strategy: {self.strategy}]'
+    def __str__(self):
+        return f"[id: {self.id}, name: {self.name}, strategy: {self.strategy}]"
--- Data Base/App/Models/user.py
+++ Data Base/App/Models/user.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, String, Integer, Boolean, Float
-from App.db import Base
-from sqlalchemy.orm import relationship
+from app.extensions import db
 
+class User(db.Model):
+    __tablename__ = 'users'
 
-class User(Base):
-    __tablename__='Users'
-    id=Column(Integer,primary_keys=True,autoincrement=True)
-    username=Column(String, nullabe=False)
-    password=Column(String, nullabe=False)
-    is_active=Column(Boolean,default=True)
-    balance=Column(Float)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    balance = db.Column(db.Float, nullable=False)
+
+    def __str__(self):
+        return f"<id: {self.id}, name: {self.username}, balance: {self.balance}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "balance": self.balance
+        }
