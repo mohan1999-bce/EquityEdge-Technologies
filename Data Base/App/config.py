ac61a1f1776254196beec0753e14a46b5fcba2e2
rud-fintech <rvv220001@utdallas.edu>
2025-05-05 23:49:25 -0500

    revert 2

--- Data Base/App/config.py
+++
@@ -1,9 +0,0 @@
-def get_cnx_string():
-    username ="admin"
-    password ="admin"
-    hostname=""
-    port=""
-    database=""
-    return f"mysql+pymysql://{username}:{password}@{hostname}:{port}/{database}"
-
-
