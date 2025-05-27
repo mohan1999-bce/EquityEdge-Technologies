a383a0f18acb3ae16844b8170a69b7f9ca61452f
ViswaBonam <viswabonam@gmail.com>
2025-05-26 21:41:40 -0500

    Final changes with working portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './Componets/Navbar';
 import About from './Componets/About';
 import Market from './Componets/Market';
 import Portfolio from './Componets/Portfolio';
+import Home from './Componets/Home';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import "./styles/global.css";
 
@@ -18,7 +19,7 @@ function App(){
    isLoggedIn:false
 
   });
-  const handLeLogout =()=>{
+  const handleLogout =()=>{
     setUser({
       user: '',
       userId: -1,
@@ -30,13 +31,14 @@ function App(){
   return (
       <>
       <Router>
-       {user.isLoggedIn && <Navbar handLeLogout={handLeLogout} />}
+       {user.isLoggedIn && <Navbar handleLogout={handleLogout} />}
        <Routes>
-         <Route path="/About" element={user.isLoggedIn ? <About /> : <Googin setUser={setUser} />} />
-         <Route path="/" element={user.isLoggedIn ? <Portfolio /> : <Googin setUser={setUser} />} />
-         <Route path="/Market" element={user.isLoggedIn ? <Market /> : <Googin setUser={setUser} />} />
-         <Route path="/Portfolio" element={user.isLoggedIn ? <Portfolio /> : <Googin setUser={setUser} />} />
-       </Routes>
+        <Route path="/" element={user.isLoggedIn ? <Home user={user} onLogout={handleLogout} /> : <Googin setUser={setUser} />} />
+        <Route path="/home" element={user.isLoggedIn ? <Home user={user} onLogout={handleLogout} /> : <Googin setUser={setUser} />} />
+        <Route path="/about" element={user.isLoggedIn ? <About onLogout={handleLogout} /> : <Googin setUser={setUser} />} />
+        <Route path="/market" element={user.isLoggedIn ? <Market /> : <Googin setUser={setUser} />} />
+        <Route path="/portfolio" element={user.isLoggedIn ? <Portfolio user={user} /> : <Googin setUser={setUser} />} />
+      </Routes>
      </Router>
 
      </>
